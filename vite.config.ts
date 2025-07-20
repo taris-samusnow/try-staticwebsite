2c53353ad10eb918becf8603b3d24ef2ac4921b8
taris-samusnow <m08mm-taris@yahoo.co.jp>
2025-07-20 05:18:50 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
     host: true,
   },
   plugins: [vue()],
+  base: "/try-staticwebsite/",   // 必ず先頭と末尾にスラッシュ
+  build: {
+    outDir: "dist"      // Vite デフォルト dist/
+  }
 })
